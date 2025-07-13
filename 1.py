08ad4afcb0cfedb3752ed66686770afab2dfe476
unknown <gartyom39@gmail.com>
2025-07-13 17:05:13 +0300

    Language support

--- 1.py
+++ 1.py
@@ -1,10 +1,16 @@
 import random
+import os
+print('To set the language as english type en/Чтобы установить язык на русский, напишите ru')
+lang = input()
+text = [string.rstrip() for string in open(f'{lang}.txt', encoding='utf-8')]
+diceCode = text[1]
 while True:
-    dice = input("Введите дайсы в формате 1к6 ")
-    amount, num = map(int,dice.split('к'))
-    k=0
+    amount, edges = map(int,input(text[0] +'\n').split(diceCode))
+    os.system("cls")
+    summary=0
+    print(text[3], f'{amount}{diceCode}{edges}')
     for i in range(amount):
-        itog = random.randint(1, num)
-        k+=itog
-        print(itog)
-    print("Всего:", k)
+        throw = random.randint(1, edges)
+        summary+=throw
+        print(throw)
+    print(text[2], summary)
